203712c6c7f7296dda3079982150b45773977ec9
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-05-20 15:38:33 +0200

    added function for freeing up colors on disconnect

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let items = [];
 let size = 25;
 
 let users = [{username: null, color:"firebrick"}, {username: null, color:"darkolivegreen"}, {username: null, color:"gold"}, {username: null, color:"cornflowerblue"}]
+let currentUserColor = null;
 //Rebecka
 
 
@@ -24,14 +25,19 @@ io.on("connection", socket => {
   
   if(items.length == 0){board(items, size)} //if the board is empty we can create a new one else we dont do it so we dont overwrite the board
   console.log("connected");
-  socket.on("newUser", (username) => {io.emit("loggedIn", login(users, username))});//Rebecka, the login-function returns an object with name and color or null (game full)
+  socket.on("newUser", (username) => {io.emit("loggedIn", login(users, username))});//(Rebecka) the login-function returns an object with name and color or null (game full)
   socket.emit("currentBoard", items);// this is where we send the board to a user that just connected
   socket.on("updateTile", (update) =>{ // when a user sends that they changed a tile
     items[update.id].color = update.color; // we update our tiles on the servers list 
     console.log(items[update.id]);
     io.emit("newTile", update);// then we tell all the other users that a tile has beeen updated
   })
-
+  socket.on("join", (userObject) => currentUserColor = userObject.color)//(Rebecka) stores the color for the current user
+  socket.on("disconnect", () => {
+    console.log(currentUserColor, "disconnected");
+    let disconnectedUser = users.find(userObject => userObject.color === currentUserColor);//(Rebecka) finds the disconnected user in the array
+    disconnectedUser.username = null;//removes username so the color is now free for grabbing
+})
 });
 
 const PORT = 3000 || process.env.PORT;
