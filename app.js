fed1b42ede31c88f66328cb6f4027680e1115ed8
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-05-21 10:23:17 +0200

    changed the port in the console log

--- app.js
+++ app.js
@@ -37,4 +37,4 @@ io.on("connection", socket => {
 const PORT = process.env.PORT || 3000;
 
 
-server.listen(PORT, () => console.log("listening on port 3000"));
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
